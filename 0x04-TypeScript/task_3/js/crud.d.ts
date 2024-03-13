9a89ce897126947aa9a9bdc1ba397238df6543ff
Arinze Blessing <41776649+blessingarizona@users.noreply.github.com>
2024-03-13 23:56:30 +0100

    Delete 0x04-TypeScript/task_3/js directory

--- 0x04-TypeScript/task_3/js/crud.d.ts
+++
@@ -1,5 +0,0 @@
-import { RowElement, RowID } from './interface';
-
-export function insertRow(row: RowElement): RowID;
-export function updateRow(rowID: RowID, row: RowElement): RowID;
-export function deleteRow(rowID: RowID): void;
